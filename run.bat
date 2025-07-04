d1a3f1ff27c050c0e7e5d02198eeba8cc263609d
Sandro Costa <sandrocosta2003@gmail.com>
2025-07-04 22:21:50 +0200

    Include bin folder and run.bat for easy execution

---
+++ run.bat
@@ -0,0 +1,12 @@
+batch@echo off
+cd /d "%~dp0"
+
+REM Set paths
+set LIB_PATH=lib
+set JAVAFX_DLL_PATH=javafx-dlls
+
+set PATH=%JAVAFX_DLL_PATH%;%PATH%
+
+java --module-path "%LIB_PATH%;bin" --add-modules ShoppingCartFX --add-exports javafx.graphics/com.sun.javafx.application=ALL-UNNAMED -m ShoppingCartFX/application.Main
+
+pause
